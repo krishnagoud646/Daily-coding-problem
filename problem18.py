790f754cf0c065de504a6f9737175b79e9f98bc7
krishnagoud646 <67950294+krishnagoud646@users.noreply.github.com>
2022-05-30 12:12:08 +0530

    Create problem18.py

---
+++ problem18.py
@@ -0,0 +1,44 @@
+"""This problem was asked by Facebook.
+
+Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
+
+For example, given the string "([])[]({})", you should return true.
+
+Given the string "([)]" or "((()", you should return false."""
+
+def balanced_Brackets(expr):
+    stack = []
+
+    for char in expr:
+        if char in ["(", "{", "["]:
+ 
+            # Push the element in the stack
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            current_char = stack.pop()
+            if current_char == '(':
+                if char != ")":
+                    return False
+            if current_char == '{':
+                if char != "}":
+                    return False
+            if current_char == '[':
+                if char != "]":
+                    return False
+ 
+    # Check Empty Stack
+    if stack:
+        return False
+    return True
+ 
+ 
+# Driver Code
+if __name__ == "__main__":
+    expr = input("Enter the Brackets: ")     # {()}[]
+
+    if balanced_Brackets(expr):
+        print("Balanced")
+    else:
+        print("Not Balanced")

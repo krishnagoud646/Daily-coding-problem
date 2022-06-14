327bf9a04eaa3f6067ea4daae6a6838eae9f2356
krishnagoud646 <67950294+krishnagoud646@users.noreply.github.com>
2022-06-14 10:57:20 +0530

    Update problem24.py

--- problem24.py
+++ problem24.py
@@ -5,21 +5,34 @@ You must buy before you can sell it.
 For example, given [9, 11, 8, 5, 7, 10], you should return 5,
  since you could buy the stock at 5 dollars and sell it at 10 dollars.
 """
-
-def maxProfitFromStocks(stocks):
-    maxProfit = stocks[1] - stocks[0]   
-    for i in range(len(stocks)-1):
-        buy = stocks[i]
-        for j in range(i+1, len(stocks)):
-            sell = stocks[j]
-            if sell - buy > maxProfit:  # update maxProfit
-                maxProfit = sell - buy
-    return maxProfit
+from collections import deque
 
 
-def maxProfitFromStocks_test(stocks):
-    print(stocks, "max profit:", maxProfitFromStocks(stocks))
+def stock_max_profit(prices):
+    max = 0
+    prices = deque(prices)
+    buy_price = prices.popleft()
 
 
-if __name__ == "__main__":
-    maxProfitFromStocks_test([9, 11, 8, 5, 7, 10])  # return 5
+    for price in prices:
+
+
+        if price < buy_price:
+            buy_price = price
+
+
+        else:
+            profit = price - buy_price
+
+
+            if profit > max:
+                max = profit
+
+
+    if max:
+        return max
+
+
+prices = [9, 11, 8, 5, 7, 10]
+max_profit = stock_max_profit(prices)
+print(prices,"Max Profit:",max_profit)# return 5
